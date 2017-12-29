1446da5a80abcec5c99b6327c574b069c797cce2
Brittany Walentin <brittw@uber.com>
2017-12-29 10:31:14 -0800

    Scaffold for day 05.

---
+++ 2017/day05_maze/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/adventofcode/2017/helpers"
+)
+
+func main() {
+	fileName := "../day05_maze/input"
+	fmt.Println("reading ", fileName)
+	inputBytes := helpers.Input(fileName)
+
+	input := convertToSlice(inputBytes)
+}
+
+func convertToSlice(input []byte) []int {
+
+}
+
+func move() {
+
+}
+
+func increment(i int, maze *[]int) {
+
+}
