67d49514f24014c45c87f83ad0b81ca063c3c2ce
Brittany Walentin <brittw@uber.com>
2018-12-05 21:50:55 -0800

    Day 5 part one complete.

---
+++ 2018/05/alchemy.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("./input.txt")
+	defer f.Close()
+	if err != nil {
+		log.Fatal("problem opening file", err)
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	var polymer string
+	for scanner.Scan() {
+		polymer = scanner.Text()
+	}
+	fmt.Println(len(polymer))
+	dictionary := createDictionary()
+
+	for {
+		foundMatch := false
+
+		for _, match := range dictionary {
+			newPolymer := strings.Replace(polymer, match, "", -1)
+			if newPolymer != polymer {
+				// fmt.Println("replaced ", match)
+				foundMatch = true
+				polymer = newPolymer
+			}
+		}
+		if foundMatch == false {
+			break
+		}
+	}
+
+	fmt.Println("leftover polymer: ", len(polymer))
+
+	// reduce matching pairs until you can do no more
+	// [a:A, b:B, B:b, A:a]
+
+	// use a for loop
+	// break out when no more matches, match == false
+
+	// when find a match, excise the pair
+	// a = append(a[:matchz], a[matchZ+1:]...)
+	// make match = true
+}
+
+func createDictionary() []string {
+	dictionary := make([]string, 0)
+	const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+	for _, char := range alphabet {
+		dictionary = append(
+			dictionary,
+			strings.ToUpper(string(char))+string(char),
+			string(char)+strings.ToUpper(string(char)),
+		)
+	}
+	// fmt.Println(dictionary)
+	return dictionary
+}
