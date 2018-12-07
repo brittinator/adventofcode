1e8af3cd05cbfe3d513d7c1606451c575646c58f
Brittany Walentin <brittw@uber.com>
2018-12-06 22:39:54 -0800

    day 6 pt 1 complete.

--- 2018/06/coordinates.go
+++ 2018/06/coordinates.go
@@ -62,7 +62,7 @@ but can discard A, B, F, and C (blacklist?) as their boundaries are infinite
 */
 
 type location struct {
-	name string
+	name int // line number
 	x, y int
 }
 
@@ -76,16 +76,14 @@ func main() {
 	scanner := bufio.NewScanner(f)
 
 	locations := make([]location, 0)
-
 	var largestX, largestY int
 	smallestX := math.MaxInt64
 	smallestY := math.MaxInt64
 
 	// What is the size of the largest area that isn't infinite?
-
+	lineNumba := 1
 	for scanner.Scan() {
 		line := scanner.Text()
-		// fmt.Println(line)
 		x, err := strconv.Atoi(strings.Split(line, ", ")[0])
 		if err != nil {
 			log.Fatal("conversion ", err)
@@ -112,19 +110,19 @@ func main() {
 		// build up the location list
 		locations = append(locations,
 			location{
-				name: line,
+				name: lineNumba,
 				x:    x,
 				y:    y,
 			})
+		lineNumba++
 	}
 
-	// these are out boundaries
-	fmt.Printf("these are out boundaries: \n %v:%v -- %v:%v\n",
+	fmt.Printf("these are the boundaries: \n %v:%v -- %v:%v\n",
 		smallestX, smallestY, largestX, largestY)
 
 	// fmt.Printf("locations: ")
 	// for i, l := range locations {
-	// 	fmt.Printf("%v: %v\n", i, l)
+	// 	fmt.Printf("\n%v: %v:%v name: %v\n", i, l.x, l.y, l.name)
 	// }
 
 	// make a grid to house state of the world
@@ -132,73 +130,117 @@ func main() {
 	for i := range grid {
 		grid[i] = make([]location, largestY)
 	}
-	partOne(locations, grid, smallestX, smallestY, largestX, largestY)
-	// find the largest x, y coordinates, these will be the boundaries
-	// automatically those points and any with the same x or y coordinates are infinite so those areas are not in the running
 
+	partOne(locations, grid, smallestX, smallestY, largestX, largestY)
 }
 
 func partOne(knownLocations []location, grid [][]location,
+	// find the largest x, y coordinates, these will be the boundaries
+	// automatically those points and any with the same x or y coordinates are infinite so those areas are not in the running
 	smallestX, smallestY, largestX, largestY int) {
-	areas := make(map[location]int)
+	type areaInfo struct {
+		area     int
+		areaList []location
+	}
+
+	areas := make(map[location]areaInfo)
 	// go thru the grid one by one to see who owns that spot
 	for xCoor, loc := range grid {
 		for yCoor := range loc {
 			grid[xCoor][yCoor].x = xCoor
 			grid[xCoor][yCoor].y = yCoor
 
-			// fmt.Println(grid[xCoor][yCoor])
 			ownerLocation := findOwner(&grid[xCoor][yCoor], knownLocations)
 			// fmt.Printf("the owner of %v:%v is %v\n", xCoor, yCoor, ownerLocation.name)
-			if _, found := areas[ownerLocation]; !found {
-				areas[ownerLocation] = 1
+			if object, found := areas[ownerLocation]; !found {
+				areas[ownerLocation] = areaInfo{
+					area: 1,
+					areaList: []location{
+						location{x: xCoor, y: yCoor},
+					},
+				}
 			} else {
-				areas[ownerLocation]++
+				// add this loc to list
+				object.areaList = append(object.areaList, location{x: xCoor, y: yCoor})
+				object.area++
+				areas[ownerLocation] = object
 			}
 		}
 	}
 
-	// at this time we should know what the largest area is
+	// fmt.Println("areaList: ")
+	// for l, info := range areas {
+	// 	fmt.Printf("loc: %v area: %v\n", l.name, info.area)
+	// }
+
 	var largestArea int
-	fmt.Println(largestX, largestY)
-
-	for location, area := range areas {
-		fmt.Println(location, area)
-
-		if area > largestArea {
-			largestArea = area
+	for _, areaInfo := range areas {
+		// fmt.Println(location, areaInfo)
+		if areaInfo.area > largestArea {
+			// check to see if area includes an edge
+			setAsLargest := true
+			for _, locList := range areaInfo.areaList {
+				if locList.x == smallestX || locList.x == largestX {
+					fmt.Println("area includes an x edge; skipping ", areaInfo.area)
+					setAsLargest = false
+					break
+				}
+				if locList.y == smallestY || locList.y == largestY {
+					fmt.Println("area includes an Y edge; skipping ", areaInfo.area)
+					setAsLargest = false
+					break
+				}
+			}
+			if setAsLargest {
+				largestArea = areaInfo.area
+			}
 		}
 	}
 
 	fmt.Println("the largest area is ", largestArea)
-	// is any part of this area at an edge?
 }
 
-func findOwner(locale *location, knownLocations []location) location {
+func findOwner(here *location, knownLocations []location) location {
 	distances := make(map[location]int, len(knownLocations))
+	// find distance from here to the known locations
 	for _, spot := range knownLocations {
-		// fmt.Printf("start: %v\nend: %v\n", locale, spot)
-		distances[spot] = manhattanDistance(*locale, spot)
+		// fmt.Printf("start: %v\nend: %v\n", here, spot)
+		distances[spot] = manhattanDistance(*here, spot)
 	}
-
-	// fmt.Println("distances: ", distances)
 
 	smallestDistance := math.MaxInt64
 	var closest location
-	for locale, currentDistance := range distances {
+	var sawDuplicate bool
+
+	for spot, currentDistance := range distances {
 		if currentDistance < smallestDistance {
-			closest = locale
+			sawDuplicate = false
+			closest = spot
 			smallestDistance = currentDistance
+			continue
+		}
+		if currentDistance == smallestDistance {
+			sawDuplicate = true
 		}
 	}
 
+	if sawDuplicate {
+		// there is no clear winner, so return blank location
+		return location{}
+	}
 	return closest
 }
 
 func manhattanDistance(start, end location) int {
 	xDistance := end.x - start.x
 	yDistance := end.y - start.y
-	// fmt.Printf("%v:%v == %v\n", start, end, int(math.Abs(float64(xDistance))+math.Abs(float64(yDistance))))
-	// ugly conversions I know
-	return int(math.Abs(float64(xDistance)) + math.Abs(float64(yDistance)))
+	// fmt.Printf("%v:%v == %v\n", start, end, absolute(xDistance) + absolute(yDistance))
+	return absolute(xDistance) + absolute(yDistance)
+}
+
+func absolute(val int) int {
+	if val < 0 {
+		return val * -1
+	}
+	return val
 }
