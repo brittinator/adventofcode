d40a06dd04ade245f1ee2a4d507c426306925e96
brittinator <brittany.walentin@gmail.com>
2020-12-05 10:29:53 -0800

    day 5 pt 2 done

--- 2020/d05/day5.py
+++ 2020/d05/day5.py
@@ -1,106 +1,44 @@
 from helpers.read_input import *
 
-# row 
-# 0-127
-# F = lower (0-63)
-# B = higher upper (64-127)
-
-# col
-# 0-7
-# L lower left
-# R upper right
-
-# final thing: row *8 + col
-
-def row_search(input, lo, hi):
-    if lo == hi or len(input) == 0:
+def search(input, lo, hi):
+    # print(input, lo, hi)
+    mid = (hi+lo)//2
+    if len(input) == 0:
         return lo
 
     for i, char in enumerate(input):
-        mid = (hi+lo)//2
-
-        if char == "L":
-            return row_search(input[i+1:], lo, mid-1)
-        elif char == "R":
-            return row_search(input[i+1:], mid+1, hi)
+        if char == "L" or char =="F":
+            return search(input[i+1:], lo, mid)
         else:
-            exit()
-    print("at end, returning with nothing")
-
-
-def col_search(input, lo, hi):
-    if lo == hi or len(input) == 0:
-        return lo
-
-    for i, char in enumerate(input):
-        mid = (hi+lo)//2
-
-        if char == "F":
-            return col_search(input[i+1:], lo, mid-1)
-
-        elif char == "B":
-            return col_search(input[i+1:], mid+1, hi)
-        else:
-            exit()
-
-    print("at end, returning with nothing")
-
+            return search(input[i+1:], mid+1, hi)
 
 def boarding_seat(bpass):
-    col = col_search(bpass[0:7], 0, 127)
-
-    row = row_search(bpass[-3:], 0, 7)
-    # print("row is: ", row)
+    col = search(bpass[0:7], 0, 127)
+    row = search(bpass[-3:], 0, 7)
 
     return col*8 + row
 
-def find_seat(passes):
-    seats = []
-
-    for bpass in passes:
-        seat = boarding_seat(bpass)
-        seats.append(seat)
-    seats.sort()
-
-    print('seats ',seats)
-    supposed_seats = []
-    min = seats[0]
-    max = seats[-1]
-    for i in range(min, max+1):
-        supposed_seats.append(i)
-
-    print("SUPPOSED", len(supposed_seats))
-
-    print("HERE")
-    # return set(range(min, max+1)).difference(seats)
-
-    # print("SEATS: ", len(seats))
-    # prev = seats[0]
-    for i in range(min, max+1):
-        if i not in seats:
-            return i
-
+def find_seat(seats):
+    return set(range(seats[0], seats[-1])).difference(seats)
 
 def binary_boarding(passes):
+    seats = []
     highest_seat = 0
     for bpass in passes:
         seat = boarding_seat(bpass)
-        print(seat, highest_seat)
+        seats.append(seat)
         if seat > highest_seat:
             highest_seat = seat
     
-    return highest_seat
-
+    return seats, highest_seat
 
 def test():
-    assert col_search("BFB", 0, 4) == 3
     test_input = "bfffbbfrrr".upper()
-    assert col_search(test_input, 0, 127) == 70
+    assert search(test_input[:-3], 0, 127) == 70
 
-    assert row_search("RRR", 0, 7) ==  7
+    assert (search("RRR", 0, 7) == 7), search("RRR", 0, 7)
 
     result = boarding_seat(test_input)
-    print("result: ", result)
     assert(result) == 567
 
     assert(boarding_seat("BBFFBBFRLL")) == 820
@@ -111,6 +49,9 @@ def run():
     test()
     
     lines = read_line_input("d05")
-    print("highest: ", binary_boarding(lines))
+    seats, bb = binary_boarding(lines)
+    print("highest: ", bb)
 
-    # print(find_seat(read_line_input("d05")))
+    # print(sorted(seats))
+
+    print(find_seat(sorted(seats)))
