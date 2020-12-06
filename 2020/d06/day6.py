b53944195ed3c52d9d3f97a38acb4a51710921c6
brittinator <brittany.walentin@gmail.com>
2020-12-06 12:59:19 -0800

    Finally use a lambda for something

--- 2020/d06/day6.py
+++ 2020/d06/day6.py
@@ -1,29 +1,19 @@
-
 from helpers.read_input import *
 
-
 def get_all_count(unique, group_count):
-    count = 0
-    # get the key with a value of group_length
-    for _, v in unique.items():
-        if v == group_count:
-            count += 1
-
-    return count
-
+    all_count = filter(lambda v: v == group_count, unique.values())
+    
+    return len(list(all_count))
 
 def customs(lines):
     counts = []
-    everyone_counts = []
+    everyone_has_these_counts = []
 
     unique = {}
     group_count = 0
     for answers in lines:
         if len(answers) == 0 :
-            # end of group
-           #  print("U", len(unique), unique, group_count)
-            
-            everyone_counts.append(get_all_count(unique, group_count))
+            everyone_has_these_counts.append(get_all_count(unique, group_count))
             counts.append(len(unique))
 
             unique = {}
@@ -33,24 +23,12 @@ def customs(lines):
         for answer in answers:
             unique[answer] = unique.get(answer, 0) +1
 
-    everyone_counts.append(get_all_count(unique, group_count))
+    everyone_has_these_counts.append(get_all_count(unique, group_count))
     counts.append(len(unique))
 
-
-    return sum(counts), sum(everyone_counts)
-
-
-
-
-def test():
-    assert(True is True)
-
-    
-    print("ASSERT done")
+    return sum(counts), sum(everyone_has_these_counts)
 
 def run():
-    test()
-
     lines = read_line_input("d06")
 
     print(customs(lines))
